8333ca0b152cdc202efbe5dca0aa0a2b93ad0c31
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-11 18:33:42 -0400

    Insert prop info into cards

--- src/app/Components/Article.jsx
+++ src/app/Components/Article.jsx
@@ -1,10 +1,13 @@
-export default function Article(props) {
+import styles from './article.module.css'
+
+export default function Article({ article }) {
+  console.log(article)
   return(
-    <article>
+    <article className={styles.card}>
       <img/>
-      <h3>Title</h3>
-      <p>by someone</p>
-      <p>lalala</p>
+      <h3>{article.title}</h3>
+      <p>{article.byline}</p>
+      <p>{article.abstract}</p>
     </article>
   )
 }
