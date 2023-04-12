eec3abcb820ef7cfa9d86a1dd91862a851f530dd
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-12 15:49:43 -0400

    Add skeleton for modal

--- src/app/Components/Modal.jsx
+++ src/app/Components/Modal.jsx
@@ -1,8 +1,22 @@
-
-export default function Modal() {
+import globals from '../globals.css'
+import styles from './modal.module.css'
+export default function Modal({ article }) {
   return (
-    <div>
-      
+    <div className={globals.modalBackground}>
+      <div className={styles.modalContainer}>
+        <button> X </button>
+        <p>image</p>
+        <div>
+          <h1>title</h1>
+          <p>byline</p>
+          <p>published date</p>
+        </div>
+        <div>
+          <p>url</p>
+          <p>abstract</p>
+        </div>
+        <p></p>
+      </div>
     </div>
   )
 }
