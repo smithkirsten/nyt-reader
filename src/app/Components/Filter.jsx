eea67d4d96a665e4bfa22e9f94ed8f535bc0eb18
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-12 13:21:03 -0400

    Add onChange functionality

--- src/app/Components/Filter.jsx
+++ src/app/Components/Filter.jsx
@@ -1,10 +1,18 @@
+'use client'
+import { useState, useEffect } from "react"
+
+export default function Filter({ handleFilter }) {
+  [selection, setSelection] = useState('')
 
 
-export default function Filter() {
-
+  const handleChange = (value) => {
+    console.log(value)
+    setSelection(value)
+    handleFilter(value)
+  }
   return(
     <div>
-      <select value={selection}>
+      <select value={selection} onChange={(e) => {handleChange(e.value)}}>
         <option value={'europe'}>Europe</option>
         <option value={'asia'}>Asia</option>
         <option value={'middleeast'}>Middle East</option>
