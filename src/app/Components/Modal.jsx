cbcd256e30f4f4b3a8ee921f1253519538427206
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-13 11:40:25 -0400

    Change divs for styling and fix url

--- src/app/Components/Modal.jsx
+++ src/app/Components/Modal.jsx
@@ -1,25 +1,25 @@
-import globals from '../globals.css'
 import styles from './modal.module.css'
 import Link from 'next/link'
 
 export default function Modal({ article, xArticle }) {
   return (
-    <div className={globals.modalBackground}>
+    <main className={styles.modalBackground}>
       <div className={styles.modalContainer}>
+        <header className={styles.buttonHeader}>
+          <button onClick={() => xArticle()} className={styles.xButton}> X </button>
+        </header>
         <div>
-          <button onClick={() => xArticle()}> X </button>
+          <img src={article.multimedia[0].url} alt={article.multimedia[0].caption} className={styles.image}/>
+          <Link href={article.url}><button className={styles.readButton}>Read the Full Article</button></Link>
         </div>
-        <img src={article.multimedia[0].url} alt={article.multimedia[0].caption} className={styles.image}/>
         <div>
           <h2>{article.title}</h2>
-          <p>{article.byline}</p>
-          <p>{article.published_data}</p>
+          <p className={styles.byline}>{article.byline}</p>
         </div>
         <div>
-          <p>{article.abstract}</p>
+          <p className={styles.abstract}>{article.abstract}</p>
         </div>
-        <Link href={`http://${article.url}`}><button>Read the Full Article</button></Link>
       </div>
-    </div>
+    </main>
   )
 }
