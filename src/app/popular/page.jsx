99506dd2d5f7992b71160a391a5c845a0e2794d0
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-13 13:45:18 -0400

    Add framework for popular page

---
+++ src/app/popular/page.jsx
@@ -0,0 +1,80 @@
+'use client'
+
+import Image from 'next/image'
+import { Noticia_Text } from 'next/font/google'
+import styles from './page.module.css'
+import globals from 'src/app/globals.css'
+import { useEffect, useState } from 'react'
+import { getArticles } from '../util'
+import Article from './Components/Article'
+import Filter from './Components/Filter'
+import Modal from './Components/Modal'
+
+const noticia = Noticia_Text({ 
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'] 
+})
+
+export default function popular() {
+
+const [articles, setArticles] = useState([])
+const [filtered, setFiltered] = useState([])
+const [error, setError] = useState({})
+const [modal, setModal] = useState({})
+
+useEffect(() => {
+  if(!articles.length){
+    (async () => {
+      const data = await getArticles()
+      data.results ?
+        setArticles(data.results) :
+        setError(error)
+    })()
+  }
+}, [])
+
+const handleFilter = (selection) => {
+  console.log('filter for ', selection)
+  selection === 'world' ?
+    setFiltered([]) :
+    setFiltered(articles.filter(article => article.subsection === selection))
+}
+
+const displayArticle = (article) => {
+  setModal(article)
+}
+
+const xArticle = () => {
+  console.log('modal off')
+  setModal({})
+}
+
+const determineCards = () => {
+  if(filtered.length) {
+    console.log('filtered cards rendering')
+    return filtered.map(article => <Article key={article.url} displayArticle={displayArticle} article={article} />)
+  } else if(articles.length) {
+    return articles.map(article => <Article key={article.url} displayArticle={displayArticle} article={article} />)
+  } else {
+    return <p>whoops, dog got the paper</p>
+  }
+}
+
+  return (
+    <>
+    { modal.title ? 
+      <Modal article={modal} xArticle={xArticle}/> :
+      <main className={globals.main}>
+        <div>
+          <h2>Top News Stories from the World Today</h2>
+          <Filter handleFilter={handleFilter} />
+        </div>
+        <section className={styles.articleSection}>
+          {determineCards()}
+        </section>
+      </main>
+    }
+    </>
+  )
+}
