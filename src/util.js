0c3c06994bfde6dec31309d0841a36c1268c9c5a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-13 13:42:58 -0400

    Make url dynamic for api call

--- src/util.js
+++ src/util.js
@@ -1,5 +1,8 @@
-export const getArticles = async() => {
-  const url = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.NEXT_PUBLIC_NYT_ID}`
+export const getArticles = async(type) => {
+  let url
+  type === 'popular' ? 
+    url = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${process.env.NEXT_PUBLIC_NYT_ID}` :
+    url = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.NEXT_PUBLIC_NYT_ID}`
    try {
     const response = await fetch(url)
     const data = await response.json()
