6c4eecb920a611f930bbac238879beef283b2aa0
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-04-13 14:15:57 -0400

    Add filter options for Science

--- src/app/Components/Filter.jsx
+++ src/app/Components/Filter.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from "react"
 
-export default function Filter({ handleFilter }) {
+export default function Filter({ handleFilter, type }) {
   const [selection, setSelection] = useState('')
 
 
@@ -12,15 +12,29 @@ export default function Filter({ handleFilter }) {
   }
   return(
     <form>
-      <div>
-        <select value={selection} onChange={(e) => {handleChange(e.target.value)}}>
-          <option value={'world'}>World</option>
-          <option value={'americas'}>Americas</option>
-          <option value={'asia'}>Asia</option>
-          <option value={'europe'}>Europe</option>
-          <option value={'middleeast'}>Middle East</option>
-        </select>
-      </div>
+      {type === 'world' &&
+        <div>
+          <select value={selection} onChange={(e) => {handleChange(e.target.value)}}>
+            <option value={'world'}>World</option>
+            <option value={'americas'}>Americas</option>
+            <option value={'asia'}>Asia</option>
+            <option value={'europe'}>Europe</option>
+            <option value={'middleeast'}>Middle East</option>
+          </select>
+        </div>
+      }
+      {type === 'science' &&       
+        <div>
+          <select value={selection} onChange={(e) => {handleChange(e.target.value)}}>
+            <option value={'science'}>All Science</option>
+            <option value={'arts'}>Art & Media</option>
+            <option value={'health'}>Health</option>
+            <option value={'space'}>Space</option>
+            <option value={'climate'}>Climate</option>
+            <option value={'us'}>US & Politics</option>
+          </select>
+        </div>
+      }
     </form>
   )
 }
